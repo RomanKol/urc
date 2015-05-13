326995de25e3465f8e1f78258e5ed761f707e94b
RomanK <kollatschny.roman@web.de>
2015-05-13 14:33:37 +0200

    Sabine App: new JSON, added weather detail
    new JSON structure,
    weather Detail with images

--- sabine/scripts/app.js
+++ sabine/scripts/app.js
@@ -133,9 +133,9 @@ function getData() {
 function initializeData(data){
 	setTemperature(data.temperature);
 	setFood(data.food);
-	setWeather(data.weather);
+	setWeather(data.forecast);
 	setStresslevel(data.stresslevel);
-	setLoudness(data.loudness);
+	setNoise(data.noise);
 	setAirquality(data.airquality);
 }
 
@@ -173,18 +173,27 @@ function setFood(food){
 	//DetailView
 	list = detailEl.querySelector('.food ul');
 	list.style.width = food.length * 100 + '%';
-	itemWidth = window.getComputedStyle(detailEl.querySelector('.foodList', null)).width;
+	itemWidth = window.getComputedStyle(detailEl.querySelector('.slider', null)).width;
 	for (var i = 0; food.length > i; i++) {
-		list.appendChild(buildSlideItem(food[i], itemWidth));
+		list.appendChild(foodListItem(food[i], itemWidth));
 	};
 }
 
 // Weather Element
 // TODO
-function setWeather(weather){
-	el = document.querySelector('#barometer figcaption');
+function setWeather(forecast){
+	el = document.querySelector('#weather figcaption');
 
 	// todo
+	list = detailEl.querySelector('.weather ul');
+	list.style.width = forecast.length * 100 + '%';
+	itemWidth = window.getComputedStyle(detailEl.querySelector('.slider', null)).width;
+	console.log(itemWidth);
+	for (var i = 0; forecast.length > i; i++) {
+		list.appendChild(weatherListItem(forecast[i], itemWidth));
+	};
+
+	console.log(forecast);
 }
 
 // Stresslevel Element
@@ -193,30 +202,30 @@ function setStresslevel(stresslevel){
 	// Tile
 	el = document.getElementById('stresslevel');
 	el.classList.remove('stresslevel-1', 'stresslevel-2', 'stresslevel-3');
-	el.classList.add('stresslevel-' + stresslevel);
+	el.classList.add('stresslevel-' + stresslevel.indoor);
 
 	// DetailView
-	detailEl.querySelector('.stresslevel h2').innerText = 'Lorem Ipsum set dolor et ament sum: ' + stresslevel;
+	detailEl.querySelector('.stresslevel h2').innerText = stresslevel.message;
 }
 
-// Loudness Element
-function setLoudness(loudness){
+// noise Element
+function setNoise(noise){
 
 	// Tile
-	el = document.getElementById('loudness');
-	el.querySelector('data').innerText = loudness.indoor + 'dB';
-	el.classList.remove('loudness-low', 'loudness-medium', 'loudness-height');
+	el = document.getElementById('noise');
+	el.querySelector('data').innerText = noise.indoor + 'dB';
+	el.classList.remove('noise-low', 'noise-medium', 'noise-height');
 
-	if(loudness.indoor <= 63){
-		el.classList.add('loudness-low');
-	} else if(loudness.indoor > 63 && loudness.indoor < 81){
-		el.classList.add('loudness-medium');
-	} else if(loudness.indoor >= 81){
-		el.classList.add('loudness-high');
+	if(noise.indoor <= 63){
+		el.classList.add('noise-low');
+	} else if(noise.indoor > 63 && noise.indoor < 81){
+		el.classList.add('noise-medium');
+	} else if(noise.indoor >= 81){
+		el.classList.add('noise-high');
 	}
 
 	// DetailView
-	detailEl.querySelector('.loudness data').innerText = loudness.indoor + ' db';
+	detailEl.querySelector('.noise data').innerText = noise.indoor + ' db';
 }
 
 // Airquality Element
@@ -225,13 +234,13 @@ function setAirquality(airquality){
 	// Tile
 	el = document.getElementById('airquality');
 	el.classList.remove('airquality-1', 'airquality-2', 'airquality-3');
-	el.classList.add('airquality-' + airquality);
+	el.classList.add('airquality-' + airquality.indoor);
 
 	// DetailView
-	detailEl.querySelector('.airquality h2').innerText = 'Lorem Ipsum set dolor et ament sum: ' + airquality;
+	detailEl.querySelector('.airquality h2').innerText = airquality.message;
 }
 
-function buildSlideItem(data, itemWidth){
+function foodListItem(data, itemWidth){
 	item = document.createElement('li');
 	item.style.width = itemWidth;
 	figure = document.createElement('figure');
@@ -251,6 +260,37 @@ function buildSlideItem(data, itemWidth){
 	return item
 }
 
+function weatherListItem(data, itemWidth){
+	item = document.createElement('li');
+	//item.style.width = itemWidth;
+	figure = document.createElement('figure');
+	figure.classList.add('slide');
+	h2Day = document.createElement('h2');
+	date = new Date(data.dt * 1000);
+	h2Day.innerText = date.getDate() + '.' + date.getMonth();
+
+	imgWeather = document.createElement('img');
+	imgWeather.src = 'images/weather/' + data.weather.icon + '.svg';
+	figcaption = document.createElement('figcaption');
+
+	h3Temp = document.createElement('h3');
+	h3Temp.innerText = Math.floor(data.temp.max) + '°C / ' + Math.floor(data.temp.min) + '°C';
+	h3Descr = document.createElement('h3');
+	h3Descr.innerText = data.weather.description;
+	h3Rain = document.createElement('h3');
+	h3Rain.innerText = Math.floor(data.rain) + '%';
+	h3Rain.classList.add('rain');
+
+	figure.appendChild(h2Day);
+	figcaption.appendChild(h3Temp);
+	figcaption.appendChild(h3Descr);
+	figcaption.appendChild(h3Rain);
+	figure.appendChild(imgWeather);
+	figure.appendChild(figcaption);
+	item.appendChild(figure);
+
+	return item
+}
 
 // Clock
 function clock () {
